c2f2bc9839d228cad8f492293008d84795b83348
Kino Phile <mykoweb@gmail.com>
2014-01-08 16:55:31 -0800

    add confirmable module

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ActiveRecord::Base
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
-         :token_authenticatable
+         :token_authenticatable,
+         :confirmable
 end
